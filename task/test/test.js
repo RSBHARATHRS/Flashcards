667fdb4032a5d25e7db5c45d185cc6edc8009a1c
Bharath R <78311020+RSBHARATHRS@users.noreply.github.com>
2021-07-15 00:15:45 -0700

    stage2

--- task/test/test.js
+++ task/test/test.js
@@ -11,7 +11,7 @@ async function stageTest() {
     const browser = await puppeteer.launch({
         headless: false,
         defaultViewport: null,
-        args:['--start-maximized']
+        args: ['--start-maximized']
     });
 
     const page = await browser.newPage();
@@ -22,24 +22,200 @@ async function stageTest() {
     await sleep(1000);
 
     let result = await hs.testPage(page,
+        //test1
+        /*
+        1)Checks existence of h1 element on the page.
+        2)Checks that there is text inside of h1
+        3)Checks that font !== serif | Times New Roman
+        * */
         () => {
             let h1 = document.body.getElementsByTagName("h1");
 
-            if (h1.length === 0) return hs.wrong("There should be h1 element on the page");
-            if (h1.length > 1) return hs.wrong("There should be only one h1 element on the page.")
-            if (! h1[0].innerText) return hs.wrong("The h1 element should contain text.")
+            if (h1.length === 0) return hs.wrong("There should be h1 element on the page.");
+            if (h1.length > 1) return hs.wrong("There should be only one h1 element on the page.");
+            if (!h1[0].innerText) return hs.wrong("The h1 element should contain text.");
+
+            let font = window.getComputedStyle(h1[0]).fontFamily;
+            if (font === '"serif"' || font === '"Times New Roman"') return hs.wrong("The text inside the h1 element should have a font different from 'serif' and 'Times New Roman'.");
 
             return hs.correct()
         },
+        //#test2
+        /*
+        Two correct cases:
+        1.
+        1) Finds element with 9 divs
+        2) Checks if it has CSS property display: flex/ grid
+        2.
+        1) Finds element with 3 divs, each of 3 divs contains 3 divs inside
+        2) For each of 3 divs checks, that there are properties display: flex flex-direction: row
+         */
+        () => {
+            let divs = document.body.getElementsByTagName("div");
+            let blocksCounter = 0;
+
+            for (let div of divs) {
+                if (div.children.length === 9) {
+                    const display = window.getComputedStyle(div).display;
+                    if (display && (display.toLowerCase() === 'flex' || display.toLowerCase() === 'grid')) {
+                        return hs.correct();
+                    } else {
+                        return hs.wrong("The CSS property display: flex or display: grid should be set to the element with 9 div elements inside.");
+                    }
+                } else if (div.children && div.children.length === 3 &&
+                    div.children[0].children && div.children[0].children.length === 3) {
+                    for (let divBlockOfThree of div.children) {
+                        const display = window.getComputedStyle(divBlockOfThree).display;
+                        const flexDirection = window.getComputedStyle(divBlockOfThree).flexDirection;
+                        if (display && display.toLowerCase() === 'flex' &&
+                            flexDirection && flexDirection.toLowerCase() === 'row') {
+                            blocksCounter++;
+                        } else {
+                            return hs.wrong("The CSS property display: flex and flex-direction: row should be set to the element with 3 div elements inside.");
+                        }
+                    }
+                }
+            }
+
+            return blocksCounter === 3 ? hs.correct() : hs.wrong("There should be an element with 9 div elements inside.");
+        },
+        //#test3
+        /*
+        2 correct cases
+        *In both cases test tries to find property font on different levels
+        1.
+        1) Finds element with 9 divs
+        2) Checks if it(or each of cards) has CSS property font !== serif | Times New Roman
+        2.
+        1) Finds element with 3 divs, each of 3 divs contains 3 divs inside
+        2) Checks if it(or each of its children or each of cards) has CSS property font !== serif | Times New Roman
+        */
         () => {
             let divs = document.body.getElementsByTagName("div");
             let k = 0;
             for (let div of divs) {
-                if ((div.children.length === 1 && div.children[0].tagName.toLowerCase() === 'p') && div.innerText)
-                    k++;
+                if (div.children.length === 9) {
+                    const font = window.getComputedStyle(div).font
+                    if (font !== '"serif"' || font !== '"Times New Roman"') {
+                        return hs.correct();
+                    }
+                    for (let card of Array.from(div.children)) {
+                        if (card.children[0] && card.children[0].tagName.toLowerCase() === 'p') {
+                            let font = window.getComputedStyle(div.children[0]).fontFamily;
+                            if (font === '"serif"' || font === '"Times New Roman"') {
+                                return hs.wrong("Text on cards should have font different from 'serif' and 'Times New Roman'");
+                            } else {
+                                k++;
+                            }
+                        } else {
+                            return hs.wrong("The structure should be the same as given in the step and all text on the cards should be in 'p' element");
+                        }
+                    }
+                } else if (div.children && div.children.length === 3 &&
+                    div.children[0].children && div.children[0].children.length === 3) {
+                    const font = window.getComputedStyle(div).font;
+                    if (font !== '"serif"' || font !== '"Times New Roman"') {
+                        return hs.correct();
+                    }
+                    for (let divBlockOfThree of div.children) {
+                        const font = window.getComputedStyle(divBlockOfThree).font;
+                        if (font !== '"serif"' || font !== '"Times New Roman"') {
+                            return hs.correct();
+                        }
+                        for (let card of divBlockOfThree.children) {
+                            const font = window.getComputedStyle(card).font;
+                            if (font !== '"serif"' || font !== '"Times New Roman"') {
+                                k++;
+                            } else {
+                                return hs.wrong("Text on cards should have font different from 'serif' and 'Times New Roman'");
+                            }
+                        }
+
+                    }
+                }
             }
 
-            return k === 9 ? hs.correct() : hs.wrong("There should be 9 div elements with text inside 'p' element.");
+
+            return k !== 9 ? hs.wrong("9 div elements should contain p element.") : hs.correct();
+        },
+        //#test4
+        /*
+        1)Checks that cards form a table 3x3 (for 2 cases)
+         */
+        () => {
+            let divs = document.body.getElementsByTagName("div");
+            let cardsH = [];
+            let cardsL = [];
+            for (let div of divs) {
+                if (div.children && div.children.length === 9) {
+                    for (let cardDiv of div.children) {
+                        let styles = window.getComputedStyle(cardDiv);
+                        if (styles.width === styles.height) {
+                            let left = cardDiv.getBoundingClientRect().left;
+                            let top = cardDiv.getBoundingClientRect().top;
+                            if (!cardsH.includes(top)) {
+                                cardsH.push(top);
+                            }
+                            if (!cardsL.includes(left)) {
+                                cardsL.push(left);
+                            }
+
+                            if (!styles.backgroundColor && !styles.backgroundImage)
+                                return hs.wrong("Each card should have a background.")
+                        }
+                    }
+                } else if (div.children && div.children.length === 3 &&
+                    div.children[0].children && div.children[0].children.length === 3) {
+                    for (let divBlockOfThree of div.children) {
+                        for (let cardDiv of divBlockOfThree.children) {
+                            let styles = window.getComputedStyle(cardDiv);
+                            if (styles.width === styles.height) {
+                                let left = cardDiv.getBoundingClientRect().left;
+                                let top = cardDiv.getBoundingClientRect().top;
+                                if (!cardsH.includes(top)) {
+                                    cardsH.push(top);
+                                }
+                                if (!cardsL.includes(left)) {
+                                    cardsL.push(left);
+                                }
+
+                                if (!styles.backgroundColor && !styles.backgroundImage)
+                                    return hs.wrong("Each card should have a background.")
+                            }
+                        }
+                    }
+                }
+            }
+
+            return cardsH.length === 3 && cardsL.length === 3 ? hs.correct() :
+                hs.wrong("The cards should form a table 3x3 and each of them should have equal width and height.")
+        },
+        //#test5
+        /*
+        1)Checks the background of the element, which contains h1 and div with 9 divs
+         */
+        () => {
+            let divs = document.body.getElementsByTagName("div");
+            for (let div of divs) {
+                let childrenElements = Array.from(div.children)
+                if (childrenElements.find(el => el.tagName.toLowerCase() === "h1")) {
+                    if (window.getComputedStyle(div).backgroundColor !== 'rgba(0, 0, 0, 0)' ||
+                        window.getComputedStyle(div).backgroundImage !== 'none') {
+
+                        return hs.correct();
+                    }
+                }
+            }
+
+            let bodyStyles = window.getComputedStyle(document.body);
+            if (bodyStyles.backgroundColor !== 'rgba(0, 0, 0, 0)' ||
+                bodyStyles.backgroundImage !== 'none') {
+
+                return hs.correct();
+            }
+
+            return hs.wrong("The element that contains header and cards should have a background.\n" +
+                "Please, make sure you set a background as CSS property.");
         }
     )
 
